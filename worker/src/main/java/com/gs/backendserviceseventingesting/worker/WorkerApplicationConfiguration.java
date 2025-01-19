e9f6f027e24eb7baebcd97b67c1469e788ebd658
Gavin Shaw <gavshaw+github@gmail.com>
2025-01-19 22:05:26 +1000

    Setup worker service to listen to queue and act on messages
    - Worker service listens to the RabbitMQ queue and process each event /
      message that the api service submits.
    - For now the worker prints out to the console indicating it has seen
      the message.

--- worker/src/main/java/com/gs/backendserviceseventingesting/worker/WorkerApplicationConfiguration.java
+++ worker/src/main/java/com/gs/backendserviceseventingesting/worker/WorkerApplicationConfiguration.java
@@ -2,14 +2,72 @@ package com.gs.backendserviceseventingesting.worker;
 
 import com.gs.backendserviceseventingesting.worker.db.GameEventsRepository;
 import com.gs.backendserviceseventingesting.worker.service.QueueProcessingService;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WorkerApplicationConfiguration {
 
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitHost;
+
+    @Value("${spring.rabbitmq.username}")
+    private String rabbitUsername;
+
+    @Value("${spring.rabbitmq.password}")
+    private String rabbitPassword;
+
+    @Value("${spring.rabbitmq.template.queue}")
+    private String rabbitQueue;
+
+    @Value("${spring.rabbitmq.template.exchange}")
+    private String rabbitExchange;
+
+    @Value("${spring.rabbitmq.template.routing-key}")
+    private String rabbitRoutingKey;
+
     @Bean
     public QueueProcessingService queueProcessingService(GameEventsRepository eventsRepository) {
         return new QueueProcessingService(eventsRepository);
+    }
+
+    @Bean
+    Queue eventQueue() {
+        return new Queue(rabbitQueue, true);
+    }
+
+    @Bean
+    Exchange eventExchange() {
+        return ExchangeBuilder.directExchange(rabbitExchange).durable(true).build();
+    }
+
+    @Bean
+    Binding binding() {
+        return BindingBuilder
+                .bind(eventQueue())
+                .to(eventExchange())
+                .with(rabbitRoutingKey)
+                .noargs();
+    }
+
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(rabbitHost);
+        cachingConnectionFactory.setUsername(rabbitUsername);
+        cachingConnectionFactory.setPassword(rabbitPassword);
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
     }
 }
--- worker/src/main/java/com/gs/backendserviceseventingesting/worker/service/QueueProcessingService.java
+++ worker/src/main/java/com/gs/backendserviceseventingesting/worker/service/QueueProcessingService.java
@@ -1,6 +1,10 @@
 package com.gs.backendserviceseventingesting.worker.service;
 
 import com.gs.backendserviceseventingesting.worker.db.GameEventsRepository;
+import com.gs.backendserviceseventingesting.worker.model.Event;
+import com.gs.backendserviceseventingesting.worker.model.GameEvent;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 public class QueueProcessingService {
 
@@ -8,5 +12,22 @@ public class QueueProcessingService {
 
     public QueueProcessingService(GameEventsRepository eventsRepository) {
         this.eventsRepository = eventsRepository;
+    }
+
+    @RabbitListener(queues = "${spring.rabbitmq.template.queue}")
+    public void processEvent(Message gameEvent) {
+        System.out.println("Message from rabbitmq received");
+        System.out.println("Message: " + gameEvent);
+//        eventsRepository.save(convertGameEventToEvent(gameEvent));
+    }
+
+    private Event convertGameEventToEvent(GameEvent gameEvent) {
+        return Event.builder()
+                .id(gameEvent.getEventId())
+                .player_id(gameEvent.getPlayerId())
+                .event_time(gameEvent.getTimestamp())
+                .event_code(gameEvent.getEventCode())
+                .event_desc(gameEvent.getEventDesc())
+                .build();
     }
 }
